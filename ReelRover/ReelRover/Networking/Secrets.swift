ceac48dd6dbbed6c260518dc1e52eaf124062b1e
Galen <galenqq@gmail.com>
2024-01-31 02:02:11 +1100

    Added Secrets

---
+++ ReelRover/ReelRover/Networking/Secrets.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+final class Secrets {
+    static let accessTokenAuthTMBD = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDJkOWRiOTJhNmJiNThhNzYxZmFjZTRiYTJmY2MyNiIsInN1YiI6IjY1Yjc1YTZkZDU1YzNkMDE3Y2ZhYmU5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wX3UaNKliYImjzXdC_h2LYRgdzOHSREfsirQ7rH0mdI"
+}
+
